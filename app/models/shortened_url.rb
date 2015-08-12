96a340dd3de464ece5f016446f71ee3c7601d7fa
Chris & Peter <chrispeter@chrispeter.com>
2015-08-12 15:04:42 -0400

    Add User-ShortenedUrl associations, and ShortenedUrl random_code factory methods

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,4 +1,30 @@
+require 'securerandom'
+
 class ShortenedUrl < ActiveRecord::Base
   validates :short_url, uniqueness: true
   validates :long_url, :short_url, presence: true
+
+  belongs_to :submitter,
+    class_name: 'User',
+    foreign_key: :user_id,
+    primary_key: :id
+
+  def self.random_code
+    while true
+      random_code = SecureRandom.urlsafe_base64
+      break unless ShortenedUrl.exists?(short_url: random_code)   # searches id by default
+    end
+
+    random_code
+  end
+
+  def self.create_for_user_and_long_url!(user, long_url)
+    shortened_url = ShortenedUrl.new(
+      long_url: long_url,
+      short_url: random_code,
+      user_id: user.id
+    )
+    shortened_url.save! # not create!
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   validates :email, uniqueness: true, presence: true
+
+  has_many :submitted_urls,
+    class_name: 'ShortenedUrl',
+    foreign_key: :user_id,
+    primary_key: :id
+
 end
