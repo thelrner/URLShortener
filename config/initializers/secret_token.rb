63c02931ad267d17bd702e0998e728bbcc4c6ce3
Chris & Peter <chrispeter@chrispeter.com>
2015-08-12 14:23:44 -0400

    Add User model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+URLShortener::Application.config.secret_key_base = '851e7369341f15e73969168223f5fc53ae95317057589fb7ce9e29c67b4eb73c7306fb23c7e1e3509cb0e95ca2fbba32c23b82ab52ffe12067d6c20265f20439'
